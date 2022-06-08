97660ec2db6a437b2cd7135b443083bc6a968ede
jon <basilieus@gmail.com>
2022-06-07 21:20:45 -0700

    fix tabs

--- babycache.c
+++ babycache.c
@@ -24,15 +24,15 @@
 /* HEADERS */
 
 typedef struct {
-	char *key;
-	uint32_t hash; // added a hashed key
-	char *value;
+    char *key;
+    uint32_t hash; // added a hashed key
+    char *value;
 } Entry;
 
 typedef struct {
-	int count;
-	int capacity;
-	Entry *entries;
+    int count;
+    int capacity;
+    Entry *entries;
 } Table;
 
 
@@ -55,79 +55,79 @@ int exit_oom();
 /* IMPLEMENTATION */
 
 int exit_oom() {
-	// Unable to allocate memory
-	printf("Unable to allocate memory.");
-	exit(137);  // Out of memory
+    // Unable to allocate memory
+    printf("Unable to allocate memory.");
+    exit(137);  // Out of memory
 }
 
 void print_awaiting_connections() {
-	printf("\tListening on localhost:6969\n");
-	printf("\n");
+    printf("\tListening on localhost:6969\n");
+    printf("\n");
 }
 
 Entry *create_entry(char *key, char *value) {
-	Entry *entry = (Entry *) malloc(sizeof(Entry));
-	if (entry == NULL)
-		exit_oom();
-	entry->key = key;
-	entry->value = value;
-	return entry;
+    Entry *entry = (Entry *) malloc(sizeof(Entry));
+    if (entry == NULL)
+        exit_oom();
+    entry->key = key;
+    entry->value = value;
+    return entry;
 }
 
 uint32_t create_hash(char *value) {
-	uint32_t hash = 0x42343434;
-	return hash;
+    uint32_t hash = 0x42343434;
+    return hash;
 }
 
 void table_add(Table *ht, Entry *entry) {
-	ht->entries = (Entry *) realloc(ht->entries, sizeof(Entry) * (ht->count + 1));
-	if (ht->entries == NULL)
-		exit_oom();
-	ht->entries[ht->count] = *entry;
-	ht->count++;
-	printf("inserted: %s %s\n", entry->key, entry->value);
+    ht->entries = (Entry *) realloc(ht->entries, sizeof(Entry) * (ht->count + 1));
+    if (ht->entries == NULL)
+        exit_oom();
+    ht->entries[ht->count] = *entry;
+    ht->count++;
+    printf("inserted: %s %s\n", entry->key, entry->value);
 }
 
 void print_logo() {
-	printf("\n");
-	printf("\t _           _                          _\n");
+    printf("\n");
+    printf("\t _           _                          _\n");
     printf("\t| |__   __ _| |__  _   _  ___ __ _  ___| |__   ___\n");
     printf("\t| '_ \\ / _` | '_ \\| | | |/ __/ _` |/ __| '_ \\ / _ \\\n");
     printf("\t| |_) | (_| | |_) | |_| | (_| (_| | (__| | | |  __/\n");
     printf("\t|_.__/ \\__,_|_.__/ \\__, |\\___\\__,_|\\___|_| |_|\\___|\n");
     printf("\t                   |___/\n");
-	printf("\n");
+    printf("\n");
 }
 
 
 Table *table_init() {
-	Table *ht = (Table *) malloc(sizeof(Table));
-	if (ht == NULL)
-		exit_oom();
-	ht->count = 0;
-	ht->capacity = 0;
-	ht->entries = NULL;
-	return ht;
+    Table *ht = (Table *) malloc(sizeof(Table));
+    if (ht == NULL)
+        exit_oom();
+    ht->count = 0;
+    ht->capacity = 0;
+    ht->entries = NULL;
+    return ht;
 
 }
 
 void table_free(Table *ht) {
-	free(ht->entries);
-	free(ht);
-	printf("hash table memory freed\n");
+    free(ht->entries);
+    free(ht);
+    printf("hash table memory freed\n");
 }
 
 
 int main(int argc, char *argv[]) {
-	print_logo();
-	print_awaiting_connections();
+    print_logo();
+    print_awaiting_connections();
 
-	Table *ht = table_init();
+    Table *ht = table_init();
 
-	table_add(ht, create_entry("jon", "doe"));
-	table_add(ht, create_entry("jon1", "doe1"));
-	table_add(ht, create_entry("jon2", "doe2"));
+    table_add(ht, create_entry("jon", "doe"));
+    table_add(ht, create_entry("jon1", "doe1"));
+    table_add(ht, create_entry("jon2", "doe2"));
 
-	table_free(ht);
-	return 0;
+    table_free(ht);
+    return 0;
 }
