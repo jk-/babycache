15dd24cf064efe0347cdb234b8250bfd67f40305
jon <basilieus@gmail.com>
2022-06-08 00:06:41 -0700

    Learning Log and Notes

--- babycache.c
+++ babycache.c
@@ -4,8 +4,8 @@
 
 /* VERSION: 1.0
  * --------------------------
- * Linear search seems slow.. oh, this isn't a hash table because we have
- * not hashed the keys. Linear search is O(n) and hash is O(1) since we can
+ * This isn't a hash table because we have not hashed the keys. 
+ * Linear search is O(n) and hash is O(1) at best since we can
  * do a direct look-up of the hash-ed key.
  *
  */
@@ -15,11 +15,40 @@
  * Implement hash on the Entry. Use the look-up to find by hash value.
  * Still haven't thought about capacity, conflict mitigation, inserting, etc.
  *
+ * -- LEARNING LOG --
+ * This is inductive - im not looking things up, just thinking until I get stumped.
+ *
  * I renamed things to make it more clear and added helper methods for memory, etc.
  *
- * I looked up hashing for tables and FNV-1a looks to be the best aglo for performance
+ * I looked up hashing algos and FNV-1a looks to be the best aglo for performance
  * and conflict. There is a concept called table load which is the ratio of
- * count to capacity for conflicts.
+ * count to capacity for conflicts (typical of any hashing algo and conflicts).
+ *
+ * Original doing linear search, I set the Table->count as the index of the entries
+ *      ht->entries[count] = *item;
+ * This means I can't look up a key without a linear search. 
+ * Also, if I can't put the hashkey as the index because well, it would segfault.
+ *
+ *      Q: I think the next idea is to bucket the index? Take some mod of the hash to 
+ *         see what bucket it fits in. Then, once we have the bucket, it needs to be 
+ *         a list of entries for clashes? Also - how does that work allocating a 
+ *         dynamic size of entries?
+ *
+ *      A: This is called "Separate Chaining" - this means that eventually each
+ *         bucket will resemble an unsorted linked list. O(n) lookup; not good enough.
+ *
+ * Q: What next? Random buckets?
+ *
+ * "Seperate chaining" brought me to "Open Addressing" --
+ *      Q: My main question now is what if you run out of empty buckets?
+ *      A: when we add a new entry, we grow the hash table relative to its load 
+ *      (probability of conflict) or "capacity".
+ *
+ * Im not going to store the hash - im going to hash the key on the fly and mod it.
+ * to look up the bucket location. If the key in that bucket is the same, I have a hit
+ * on a read. if its not, I have to increment ("probe sequencing") until I run out of
+ * indexes (this is the worst case scenario).
+ *
  */
 
 #define FNV_OFFSET 14695981039346656037UL
@@ -28,13 +57,13 @@
 /* HEADERS */
 
 typedef struct {
-    uint64_t hash; // added a hashed key
+    char *key;
     char *value;
 } Entry;
 
 typedef struct {
-    int count;
-    int capacity;
+    int count; 
+    int capacity;   // now we know why capacity is here
     Entry *entries;
 } Table;
 
@@ -72,7 +101,7 @@ Entry *create_entry(char *key, char *value) {
     Entry *entry = (Entry *) malloc(sizeof(Entry));
     if (entry == NULL)
         exit_oom();
-    entry->hash = hash_string(key);
+    entry->key = key;
     entry->value = value;
     return entry;
 }
@@ -87,12 +116,18 @@ static uint64_t hash_string(const char* string) {
 }
 
 void table_add(Table *ht, Entry *entry) {
+    // TODO:
+    // We need to find the key in the table before we allocate more space
+    // Since we are now going to bin the hash keys.
+    // We need to change the interface to accept key/value and not an entire entry
+    // No point in allocating an entries memory if its not added (can just replace
+    // the value if the key exists
     ht->entries = (Entry *) realloc(ht->entries, sizeof(Entry) * (ht->count + 1));
     if (ht->entries == NULL)
         exit_oom();
     ht->entries[ht->count] = *entry;
     ht->count++;
-    printf("inserted: %llu %s\n", entry->hash, entry->value);
+    printf("inserted: %s %s\n", entry->key, entry->value);
 }
 
 void print_logo() {
@@ -106,7 +141,6 @@ void print_logo() {
     printf("\n");
 }
 
-
 Table *table_init() {
     Table *ht = (Table *) malloc(sizeof(Table));
     if (ht == NULL)
@@ -115,15 +149,12 @@ Table *table_init() {
     ht->capacity = 0;
     ht->entries = NULL;
     return ht;
-
 }
 
 void table_free(Table *ht) {
     free(ht->entries);
     free(ht);
-    printf("hash table memory freed\n");
 }
-
 
 int main(int argc, char *argv[]) {
     print_logo();
