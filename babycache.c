1ca147ddac827a22ceae3e7024472ca9a7a3c814
jon <basilieus@gmail.com>
2022-06-07 21:06:39 -0700

    base for version 2

--- babycache.c
+++ babycache.c
@@ -3,78 +3,80 @@
 #include <string.h>
 
 /* VERSION: 1.0
- *
+ * --------------------------
  * Linear search seems slow.. oh, this isn't a hash table because we have
  * not hashed the keys. Linear search is O(n) and hash is O(1) since we can
- * a direct look-up of the hash-ed key
+ * do a direct look-up of the hash-ed key.
+ *
+ */
+
+/* VERSION: 2.0
+ * --------------------------
+ * Implement hash on the Entry. Use the look-up to find by hash value.
+ * Still haven't thought about capacity, conflict mitigation, inserting, etc.
+ *
+ * I renamed things to make it more clear and added helper methods for memory, etc.
  *
  */
 
 
-char *ADDRESS = "localhost";
-
-
 typedef struct {
 	char *key;
+	uint32_t hash; // added a hashed key
 	char *value;
 } Entry;
 
 typedef struct {
 	int count;
 	int capacity;
-	Entry *items;
+	Entry *entries;
 } Table;
 
 
-// search
-char *linear_search(Table *ht, char *key);
+Table *table_init();
+void table_free(Table *ht);
+void table_insert(Table *ht, char *key, char *value);
+char *table_get(Table *ht, char *key, uint32_t hash);
 
-
-// cruds
-Entry *create_item(char *key, char *value);
-void insert_item(Table *ht, Entry *item);
-void remove_item(char key[]);
+Entry *create_entry(char *key, char *value);
 
 // prints
 void print_logo();
 void print_awaiting_connections();
 
+// errors
+int exit_oom();
+
+
+//
+
+int exit_oom() {
+	// Unable to allocate memory
+	printf("Unable to allocate memory.");
+	exit(137);  // Out of memory
+}
 
 void print_awaiting_connections() {
 	printf("\tListening on localhost:6969\n");
 	printf("\n");
 }
 
-
-char *linear_search(Table *ht, char *key) {
-	int i;
-	for (i=0; i<ht->count;i++) {
-		if (strcmp(ht->items[i].key, key) == 0) {
-			return ht->items[i].value;
-		}
-	}
-	return NULL;
+Entry *create_entry(char *key, char *value) {
+	Entry *entry = (Entry *) malloc(sizeof(Entry));
+	if (entry == NULL)
+		exit_oom();
+	entry->key = key;
+	entry->value = value;
+	return entry;
 }
 
-
-Entry *create_item(char *key, char *value) {
-	Entry *item = (Entry *) malloc(sizeof(Entry));
-	if (item == NULL) {
-		exit(127);
-	}
-	item->key = key;
-	item->value = value;
-	return item;
-}
-
-void insert_item(Table *ht, Entry *item) {
-	ht->items = (Entry *) realloc(ht->items, sizeof(Entry) * (ht->count + 1));
-	if (ht->items == NULL) {
-		exit(127);
-	}
+void table_add(Table *ht, Entry *entry) {
+	ht->entries = (Entry *) realloc(ht->entries, sizeof(Entry) * (ht->count + 1));
+	if (ht->entries == NULL)
+		exit_oom();
+	ht->entries[ht->count] = *entry;
 	ht->count++;
-	ht->items[ht->count - 1] = *item;
-	printf("inserted: %s %s\n", item->key, item->value);
+	printf("inserted: %s %s\n", entry->key, entry->value);
 }
 
 void print_logo() {
@@ -88,21 +90,34 @@ void print_logo() {
 	printf("\n");
 }
 
+
+Table *table_init() {
+	Table *ht = (Table *) malloc(sizeof(Table));
+	if (ht == NULL)
+		exit_oom();
+	ht->count = 0;
+	ht->capacity = 0;
+	ht->entries = NULL;
+	return ht;
+
+}
+
+void table_free(Table *ht) {
+	free(ht->entries);
+	free(ht);
+	printf("hash table memory freed");
+}
+
+
 int main(int argc, char *argv[]) {
 	print_logo();
 	print_awaiting_connections();
-	Table *ht = (Table *) malloc(sizeof(Table));
-	if (ht == NULL) {
-		exit(127);
-	}
-	ht->count = 0;
-	ht->capacity = 0;
-	ht->items = NULL;
-	insert_item(ht, create_item("jon", "doe"));
-	insert_item(ht, create_item("jon1", "doe1"));
-	insert_item(ht, create_item("jon2", "doe2"));
-	printf("%s\n", linear_search(ht, "jon1"));
-	free(ht->items);
-	free(ht);
+
+	Table *ht = table_init();
+	table_add(ht, create_entry("jon", "doe"));
+	table_add(ht, create_entry("jon1", "doe1"));
+	table_add(ht, create_entry("jon2", "doe2"));
+
+	table_free(ht);
 	return 0;
 }
